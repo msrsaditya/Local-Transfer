e1e47498119e62237bfafa702bfb8b49ef234094
Shashank Aditya <msrsaditya@gmail.com>
2025-05-31 18:55:30 +0530

    Update get.sh

--- macOS/get.sh
+++ macOS/get.sh
@@ -8,20 +8,9 @@ mkdir -p "${folder}"
 
 nc -l -w 60 "${port}" | {
   IFS= read -r file
-  read -r expected_bytes
-
   if [[ -z "${file}" ]]; then
-      tar --no-same-owner --warning=no-unknown-keyword -xpf - -C "${folder}"
-      exit 0
-  fi
-
-  path="${folder}/${file}"
-  cat > "${path}"
-
-  actual_bytes=$(stat -f%z "${path}")
-
-  if [[ "${expected_bytes}" -ne "${actual_bytes}" ]]; then
-    echo "Error: Incomplete Transfer" >&2
-    exit 1
+    tar --no-same-owner --warning=no-unknown-keyword -xpf - -C "${folder}"
+  else
+    cat > "${folder}/${file}"
   fi
 }
