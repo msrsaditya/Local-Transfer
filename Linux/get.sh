17112273965ad4528fe748d5e5bbdaf36f6aaa6d
Shashank Aditya <msrsaditya@gmail.com>
2025-09-09 17:24:47 +0530

    Update get.sh

--- Linux/get.sh
+++ Linux/get.sh
@@ -9,7 +9,7 @@ mkdir -p "${folder}"
 nc -l -w 60 "${port}" | {
   IFS= read -r file
   if [[ -z "${file}" ]]; then
-    tar --no-same-owner --warning=no-unknown-keyword -xpf - -C "${folder}"
+    tar --no-same-owner --warning=no-unknown-keyword --blocking-factor=8192 -xpf - -C "${folder}"
   else
     cat > "${folder}/${file}"
   fi
